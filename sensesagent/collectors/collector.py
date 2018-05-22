885f043f7a0c3de74028f5de01cb02d67d2eacb2
Jason Viloria <jnvilo@gmail.com>
2018-05-22 17:41:43 +0200

    added template loader

--- sensesagent/collectors/collector.py
+++ sensesagent/collectors/collector.py
@@ -10,6 +10,7 @@ from future.utils import iteritems
 
 import platform
 import logging 
+from pathlib import Path 
 
 from sensesagent import log
 
@@ -18,6 +19,11 @@ if platform.architecture()[0] == '64bit':
 else:
     MAX_COUNTER = (2 ** 32) - 1
 
+class TemplateLoader(object): 
+    """Loads template"""
+    
+    def __init__(self):
+        pass
     
 
 class Collector(object):
@@ -29,6 +35,8 @@ class Collector(object):
         self.metric = {}
         self.template_path = template_path
         self.template = self.load_template()
+    
+    
     
     def collect_metrics(self):
         """Implements gathering metrics"""    
@@ -53,17 +61,28 @@ class Collector(object):
         
         """
         if self.template_path == None: 
-            local_path = "conf/{}.template".format(self.__class__.__name__)
-            search_paths = ["conf/{}.template".format(self.__class__.__name__),
-                            "/etc/sensesagent/{}".format(local_path)
-                            ] 
-        
-        with open(self.template_path, "r") as f:
-            template = f.read()
-            return template
-        
-        
-    def gather_metric(self, name, value):
+            local_path = "conf/collector_templates/{}.template".format(self.__class__.__name__)
+            conf_files = [local_path,
+                          "/etc/sensesagent/conf/collector_templates/{}".format(local_path)
+                        ] 
+            
+            for conf_file in conf_files:    
+                try:
+                    with open(conf_file, "r") as f:
+                        template = f.read()
+                        return template
+                except TypeError: 
+                    if self.template_path == None: 
+                        #If we get here then we did not manage to find a template file
+                        msg = "Failed to Load Template File for {}".format(self.__class__.__name__)
+                        self.logger.fatal(msg)
+        else: 
+            #Just load what we were given then:
+            with open(self.template_path, "r") as f:
+                template = f.read()
+                return template            
+            
+def gather_metric(self, name, value):
     
         pass
     
