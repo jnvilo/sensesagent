48b6f371608b4080382468979bb80b9ef1b42b39
Jason Viloria <jnvilo@gmail.com>
2018-05-22 17:40:31 +0200

    implemented main as startup runner

--- senses.py
+++ senses.py
@@ -1,6 +1,21 @@
 import sensesagent
 import sensesagent.collectors.collector
 print(sensesagent.version) 
-import os
 
-print(os.path.dirname(os.path.realpath(__file__)))
+import os
+from pathlib import Path
+
+from sensesagent import SensesAgent
+
+
+def main():
+    """Launches the senses agent."""
+    
+    start_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    sa = SensesAgent(start_dir)
+    sa.run_collectors()
+    
+    
+if __name__ == "__main__":
+    
+    main()
