405541d44da14ab15ae15fb557359e141b19bf6f
Jason Viloria <jnvilo@gmail.com>
2018-05-25 16:15:01 +0200

    added new collectors

--- sensesagent/collectors/LoadAverageCollector.py
+++ sensesagent/collectors/LoadAverageCollector.py
@@ -10,30 +10,40 @@ from future.utils import iteritems
 
 import os
 import logging
-
+import psutil
 from multiprocessing import cpu_count
 from sensesagent.collectors.collector import Collector
 
 
 class LoadAverageCollector(Collector):
     """
-    Collects Load average 
+    Collects Load average. This collector exposes the following metrics:
+        load_1_minute - The 1 minute load average
+        load_5_minute - The 5 minute load average
+        load_15_minute - The 15 minute load average
     """       
 
     def collect_metrics(self):
         """Implements gathering the metrics and filling up our 
-        metrics object"""
+        metrics object
+        
+        """
             
         load_1_minute, load_5_minute, load_15_minute = os.getloadavg()
-        num_cpu = cpu_count()
+        cpu_percent = psutil.cpu_percent()
     
         self.add_metric("load_1_minute", load_1_minute) 
         self.add_metric("load_5_minute", load_5_minute)
         self.add_metric("load_15_minute", load_15_minute) 
-        self.add_metric("num_cpu", num_cpu )
-    
         
-   
+if __name__ == "__main__":
+    
+    lac = LoadAverageCollector()
+    
+    for key in lac.metrics.keys():
+        print(key)
+    
+    
    
     
     
--- sensesagent/collectors/collector.py
+++ sensesagent/collectors/collector.py
@@ -9,15 +9,19 @@ from future.utils import raise_from
 from future.utils import iteritems
 
 
-from jinja2 import Template
-
-
+#Std Lib imports
 import platform
 import logging 
 from pathlib import Path 
 
+#third party libs imports
+from jinja2 import Template
+from datetime import datetime
+
+#sensesagent imports
 from sensesagent import log
 from sensesagent.utils import DictionaryUtility
+from sensesagent import exceptions
 
 
 
@@ -26,42 +30,38 @@ if platform.architecture()[0] == '64bit':
 else:
     MAX_COUNTER = (2 ** 32) - 1
 
-class TemplateLoader(object): 
-    """Loads template"""
-    
-    def __init__(self):
-        pass
-    
 
 class Collector(object):
+    """
+    The base class for all collectors. 
+    """
     
     def __init__(self, template_path=None, metric=None, config=None):
         
         cls_name = self.__class__.__name__
         
-        if metric is None:
-            metric= {}
+        #if metric is None:
+        #    metric= {}
         self.logger = logging.getLogger(cls_name)
         self.logger.debug("Instantiating {}".format(cls_name))
-        self.metric_dict = metric
+        self.metric_dict = {}
         self.template_path = template_path
         self.template = self.load_template()
         self.config = config
     
         #Convert the config into a dictionary 
-        
         self.metric_dict.update( {"config": config} ) 
-        
-        
     
     @property
     def json(self):
         return self.process_template()
     
     @property 
-    def metric(self):
+    def metrics(self):
         """
-        Returns a metric object
+        Returns the metrics_dict. using this instead of directly 
+        accessing metrics_dict ensures that we have executed collect_metrics
+        first. 
         """
         # this is just a facade for collect_metrics. Will 
         # rewrite this to use a MetricObject and migrate 
@@ -69,6 +69,14 @@ class Collector(object):
         #return DictionaryUtility().to_object(self.metric_dict)
         #the metric 
         
+        if len(self.metric_dict) == 1: 
+            #Two things is possilbe. Either the implementation has no metrics
+            # or most probably we have not yet called collect_metrics
+            
+            self.collect_metrics()
+            
+        return self.metric_dict
+    
         
     def collect_metrics(self):
         """Implements gathering metrics"""    
@@ -89,8 +97,19 @@ class Collector(object):
         
         """
         if self.template_path == None: 
+            
+            #local_path is relative to the entry point file. In this case the 
+            #agent is started in senses.py at the topmost directory of the source
+            #code. 
             local_path = "conf/collector_templates/{}.template".format(self.__class__.__name__)
-            conf_files = [local_path,
+            
+            #relative_path is a path that leads to senses.py which is 2 directories
+            #above  this file. 
+            senses_path = Path(__file__).parent.parent.parent
+            relative_path = Path(senses_path, local_path)
+            
+            #TODO: Test using /etc as the path.
+            conf_files = [local_path, relative_path.as_posix(), 
                           "/etc/sensesagent/conf/collector_templates/{}".format(local_path)
                         ] 
             
@@ -99,13 +118,18 @@ class Collector(object):
                     with open(conf_file, "r") as f:
                         template = f.read()
                         return template
-                except TypeError: 
-                    if self.template_path == None: 
-                        #If we get here then we did not manage to find a template file
-                        msg = "Failed to Load Template File for {}".format(self.__class__.__name__)
-                        self.logger.fatal(msg)
+                except FileNotFoundError as e: 
+                    # Ignore the FileNotFoundError because we shall keep going 
+                    # through the rest of the conf_files search. 
+                    pass
+            #If we arrive to here then it means template not found.
+
+            cls_name = self.__class__.__name__
+            message = "Could not load template for {}".format(cls_name)
+            raise exceptions.TemplateFileNotFound(message)
         else: 
             #Just load what we were given then:
+            #TODO: This will bomb out if the provided template is not working. 
             with open(self.template_path, "r") as f:
                 template = f.read()
                 return template            
@@ -120,17 +144,23 @@ class Collector(object):
         self.collect_metrics()
         
         #The jinja2 template
-        
         json_str = Template(self.template).render(metric=self.metric_dict)
-       
         return json_str
      
     
     def add_metric(self, name, value):
         self.metric_dict.update({ name:value})
           
+
           
-          
+    def __str__(self): 
+      
+        result = ""
+        for metric in self.metrics: 
+            result = result + "{}:{}\n".format(metric, self.metrics[metric])
+        return result
+        
+        
 def gather_metric(self, name, value):
     
         pass
