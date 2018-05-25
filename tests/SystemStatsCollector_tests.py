3439d4c54ec399a76a4fd5ee96edc697e0c50b61
Jason Viloria <jnvilo@gmail.com>
2018-05-25 16:17:16 +0200

    new collecotr

---
+++ tests/SystemStatsCollector_tests.py
@@ -0,0 +1,39 @@
+import unittest 
+
+import os
+import simplejson as json
+from pathlib import Path
+
+from sensesagent.collectors.loadaverage import SystemStatsCollector
+from sensesagent import log
+
+class SystemStatsCollector(unittest.TestCase):
+    
+    def setUp(self):
+        
+        #The config path is one directory above us. 
+        
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        parent_path = Path(dir_path).parent
+        template_path = Path(parent_path, "conf/collector_templates/LoadAverageCollector.template")
+        self.lac = SystemStatsCollector(template_path=template_path.as_posix())
+    
+    def test_can_process_template(self):
+        
+        json_str = self.lac.process_template()
+        print(json_str)
+        
+        
+    def test_metric(self):
+        pass
+        
+    def test_json(self):
+        x = json.loads(self.lac.json)
+        print(x)
+        
+if __name__ == "__main__":
+    
+    unittest.main()
+    
+    
+    
