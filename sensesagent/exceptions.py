b863794af4eb5e40834679819595c0a31924c7e4
Jason Viloria <jnvilo@gmail.com>
2018-05-25 16:17:03 +0200

    add exception for File does not exist

--- sensesagent/exceptions.py
+++ sensesagent/exceptions.py
@@ -7,3 +7,9 @@ class ConfigFileNotFound(Exception):
         super().__init__(message)
 
     
+class TemplateFileNotFound(Exception):
+    def __init__(self, message):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
